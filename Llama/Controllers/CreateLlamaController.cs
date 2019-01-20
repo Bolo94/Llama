7114a1ff85a2394c1f8a1649092f597bf2fb6f57
marcos5ilva <marcos.silva79@gmail.com>
2019-01-20 11:52:36 -0700

    Added Create Llama Index, Name, and Custumization pages

---
+++ Llama/Controllers/CreateLlamaController.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Llama.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Llama.Controllers {
+    public class CreateLlamaController : Controller {
+        public IActionResult Index () {
+            return View ();
+        }
+
+        public IActionResult Name () {
+            return View ();
+        }
+
+        public IActionResult Custumization () {
+            return View ();
+        }
+    }
+}
