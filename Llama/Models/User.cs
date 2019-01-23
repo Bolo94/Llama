5d7b46035c5c5e47c3fa3d1c1018dc1697f8cafa
carlosavalle <carlosavalle@users.noreply.github.com>
2019-01-23 13:59:25 -0700

    Testing

---
+++ Llama/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Llama.Models
+{
+    public class User
+    {
+        public int IdUser { get; set; }
+        public String NickName { get; set; }
+        public String UserName { get; set; }
+        public String Password { get; set; }
+        public int Age { get; set; }
+        public Boolean Gender { get; set; }
+    }
+}
