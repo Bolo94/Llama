52032ba299bca48c76f4ac833b9dcf1bb26a7420
KCamille9 <bgi_camille@hotmail.com>
2019-01-22 18:46:41 -0700

    updated color buttons, tails buttons not working

--- Llama/wwwroot/js/site.js
+++ Llama/wwwroot/js/site.js
@@ -3,13 +3,19 @@
 
 // Write your JavaScript code.
 
-function callMagenta(){
-    document.getElementById('llama-core').setAttribute('style', 'fill: #ff00ff');
-   
-  }
-  
+
   function changeToWhite(clicked_id){
-    document.getElementById('llama-core').setAttribute('style', 'fill: #ffffff');
+    if(clicked_id == "core_white_button"){
+      document.getElementById('llama-core').setAttribute('style', 'fill: #ffffff');
+     }
+    else if(clicked_id =="eyes_white_button"){
+      document.getElementById('llama-right-eye').setAttribute('style', 'fill: #ffffff');
+      document.getElementById('llama-left-eye').setAttribute('style', 'fill: #ffffff');
+  
+    }
+    else if(clicked_id == "tail_white_button"){
+      document.getElementById('llama-tail-outer-shadow').setAttribute('style', 'fill: #fffff');
+    }
     
   }
   
@@ -29,14 +35,14 @@ function callMagenta(){
   }
   
   function changeToGreen(clicked_id){
-    if(clicked_id == "core_blue_button"){
+    if(clicked_id == "core_green_button"){ 
       document.getElementById('llama-core').setAttribute('style', 'fill: #57BB58');
      }
-    else if(clicked_id =="eyes_blue_button"){
+    else if(clicked_id =="eyes_green_button"){
       document.getElementById('llama-right-eye').setAttribute('style', 'fill: #57BB58');
       document.getElementById('llama-left-eye').setAttribute('style', 'fill: #57BB58');
     }
-    else if(clicked_id == "tail_blue_button"){
+    else if(clicked_id == "tail_green_button"){
       document.getElementById('llama-tail-outer-shadow').setAttribute('style', 'fill: #57BB58');
     }
   }
@@ -49,10 +55,11 @@ function callMagenta(){
       document.getElementById('llama-right-eye').setAttribute('style', 'fill: #5BC0DE');
       document.getElementById('llama-left-eye').setAttribute('style', 'fill: #5BC0DE');
     }
-    else if(clicked_id == "tail_light_blue_button"){
-      document.getElementById('llama-tail-outer-shadow').setAttribute('style', 'fill: #5BC0DE');
-    }
+    else if(clicked_id == "tail_blue_button"){
+      document.getElementById('llama-tail-outer-shadow').setAttribute('style', 'fill: #347AB6');
   }
+
+}
   
   
   function changeToOrange(clicked_id){
@@ -85,7 +92,35 @@ function callMagenta(){
      document.getElementById('llama-right-ear').setAttribute('style', 'fill: #D9544F');
   }
   
-  }
+}
+
+function changeToLBrown(clicked_id){
+    if(clicked_id == "core_lbrown_button"){
+        document.getElementById('llama-core').setAttribute('style', 'fill: #C77E5D');
+       }
+    else if(clicked_id =="eyes_lbrown_button"){
+        document.getElementById('llama-right-eye').setAttribute('style', 'fill: #C77E5D');
+        document.getElementById('llama-left-eye').setAttribute('style', 'fill: #C77E5D');
+      }
+    else if(clicked_id == "tail_lbrown_button"){
+        document.getElementById('llama-tail-outer-shadow').setAttribute('style', 'fill: #C77E5D');
+      }
+
+}
+
+function changeToGray(clicked_id){
+    if(clicked_id == "core_gray_button"){
+        document.getElementById('llama-core').setAttribute('style', 'fill: #C0C0C0');
+       }
+    else if(clicked_id =="eyes_gray_button"){
+        document.getElementById('llama-right-eye').setAttribute('style', 'fill: #C0C0C0');
+        document.getElementById('llama-left-eye').setAttribute('style', 'fill: #C0C0C0');
+      }
+    else if(clicked_id == "tail_gray_button"){
+        document.getElementById('llama-tail-outer-shadow').setAttribute('style', 'fill: #C0C0C0');
+      }
+
+}
   
   
   
