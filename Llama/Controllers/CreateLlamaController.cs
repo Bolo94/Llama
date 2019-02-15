b2151aeb184e9c9136706e1065d7b764d6482fa6
KCamille9 <bgi_camille@hotmail.com>
2019-02-15 08:07:40 -0700

    LlamaColor page returning hexadecimal color

--- Llama/Controllers/CreateLlamaController.cs
+++ Llama/Controllers/CreateLlamaController.cs
@@ -6,30 +6,46 @@ using System.Threading.Tasks;
 using Llama.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Llama.Controllers {
+namespace Llama.Controllers
+{
 
     /*List for form submition test */
 
-    public class CreateLlamaController : Controller {
-        public IActionResult Index () {
-            return View ();
+    public class CreateLlamaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
         }
 
-        public IActionResult Name () {
+        public IActionResult Name()
+        {
 
-            return View ();
-
-        }
-
-       
-        public IActionResult NamePageTest (LlamaName test) {
-
-           return View(test);
+            return View();
 
         }
 
-        public IActionResult Customization () {
-            return View ();
+
+        public IActionResult NamePageTest(LlamaName test)
+        {
+
+            return View(test);
+
+        }
+
+        public IActionResult Customization()
+        {
+            return View();
+        }
+
+        public IActionResult LlamaColor()
+        {
+            return View();
+        }
+
+        public IActionResult LlamaColorResult(LlamaColors llamacolor)
+        {
+            return View(llamacolor);
         }
     }
 }
