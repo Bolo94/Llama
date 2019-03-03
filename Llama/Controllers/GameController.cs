7e677a6e8eaa530bc86e3592033f8da24f45be24
Marcos Silva <marcos.silva79@gmail.com>
2019-03-03 14:40:56 -0700

    Integrated phaser to ASP.NET core project

---
+++ Llama/Controllers/GameController.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Llama.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Llama.Controllers
+{
+    public class GameController : Controller
+    {
+       public IActionResult Playground()
+        {
+            return View();
+        } 
+    }
+
+
+}
