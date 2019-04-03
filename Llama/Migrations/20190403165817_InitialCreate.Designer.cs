02ba10eba26d3603bcc2a95ada64ee3da273f99c
Marcos Silva <marcos.silva79@gmail.com>
2019-04-03 13:40:51 -0600

    User Id test

--- Llama/Migrations/20190403165817_InitialCreate.Designer.cs
+++ Llama/Migrations/20190403165817_InitialCreate.Designer.cs
@@ -10,14 +10,14 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Llama.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    [Migration("20190402193117_InitialCreate")]
+    [Migration("20190403165817_InitialCreate")]
     partial class InitialCreate
     {
         protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
-                .HasAnnotation("ProductVersion", "2.2.1-servicing-10028")
+                .HasAnnotation("ProductVersion", "2.2.2-servicing-10034")
                 .HasAnnotation("Relational:MaxIdentifierLength", 128)
                 .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
 
